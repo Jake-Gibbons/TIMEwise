21e58107f6cb9c7051123248af3c653e8f74be6b
Jake Gibbons <Jake_Gibbos@live.co.uk>
2023-06-30 07:11:56 +0100

    Initial Commit

---
+++ TimeWise/TimeWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeWiseApp.swift
+//  TimeWise
+//
+//  Created by Jake Gibbons on 30/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
